135080032407fcf1fbf3bfb26a29b73b82340a11
league <aaritrj@gmail.com>
2023-10-29 11:25:23 -0700

    started level 5 module 3

--- src/_01_Intro_To_Sockets/client/ClientGreeter.java
+++ src/_01_Intro_To_Sockets/client/ClientGreeter.java
@@ -13,9 +13,7 @@ public class ClientGreeter {
       
       //3. Surround steps 4-9 in a try-catch block that catches any IOExceptions.
     
-    	 //4. Create an object of the Socket class. When initializing the object, pass in the ip address and the port number
- 
-         //5. Create a DataOutputStream object. When initializing it, use the Socket object you created in step 4 to call the getOutputStream() method.
+    	 //4. Create an object of the Socket class. When initializing the object, pass in the ip address and the port number//5. Create a DataOutputStream object. When initializing it, use the Socket object you created in step 4 to call the getOutputStream() method.
          
          //6. Use the DataOutputStream object to send a message to the server using the writeUTF(String message) method
          
